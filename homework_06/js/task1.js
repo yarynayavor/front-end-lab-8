24b42e2d2dfe0cde3d2bddf0f2673d00aafcb945
yarynayavor <yourhurt09@gmail.com>
2018-02-08 21:08:35 +0200

    playing with es6

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,10 +1,8 @@
-var a=parseFloat(prompt("Enter length of size A:", "3"));
-var b=parseFloat(prompt("Enter length of size B:", "4"));
-var c=parseFloat(prompt("Enter length of size C:", "5"));
-var showTypeOfTriangle=findType();
-var showSquare=square();
+let a=parseFloat(prompt("Enter length of size A:", "3"));
+let b=parseFloat(prompt("Enter length of size B:", "4"));
+let c=parseFloat(prompt("Enter length of size C:", "5"));
 
-function findType() {
+let findType=()=> {
 	var typeOfTriangle;
 	if(Math.pow(c,2)===Math.pow(a,2)+Math.pow(b,2)) {
 		typeOfTriangle="right triangle";
@@ -18,16 +16,20 @@ function findType() {
 	return typeOfTriangle;
 }
 
-function square() {
+let square=()=> {
 	var p=parseFloat((a+b+c)/2);
 	var res=Math.sqrt(p*(p-a)*(p-b)*(p-c));
 	return parseFloat(res.toFixed(2));
 }
 
 if(!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+	let showTypeOfTriangle=findType();
+	let showSquare=square();
 	if((a>0 && b>0 && c>0) && (showSquare>0)) {
 		console.log(`Type of triangle is ${showTypeOfTriangle} and square is ${showSquare}`);
-	} 
+	} else {
+		console.log("Incorrect data");
+	}
 } else {
 	console.log("Incorrect data");
 }
--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,14 +1,14 @@
-var euro = parseInt(prompt("Enter amount of EURO: "));
-var dollars = parseInt(prompt("Enter amount of USD: "));
+let euro = parseInt(prompt("Enter amount of EURO: "));
+let dollars = parseInt(prompt("Enter amount of USD: "));
 
 const oneEuro = 1;
 const curEur=34.5116;
 const curDol=27.7424;
 
-var curDiff=(curEur/curDol);
-var euroToGryvnya=curEur*euro;
-var dollarsToGryvnya=curDol*dollars;
-var euroToDollars=curDiff*oneEuro;
+let curDiff=(curEur/curDol);
+let euroToGryvnya=curEur*euro;
+let dollarsToGryvnya=curDol*dollars;
+let euroToDollars=curDiff*oneEuro;
 
 if(!isNaN(euro) && !isNaN(dollars))  {
 	console.log(`${euro} euros are equal ${parseFloat(euroToGryvnya.toFixed(2))} UAH, ${dollars} dollars are equal ${parseFloat(dollarsToGryvnya.toFixed(2))} UAH, one euro is equal ${parseFloat(euroToDollars.toFixed(2))} dollars.`);
