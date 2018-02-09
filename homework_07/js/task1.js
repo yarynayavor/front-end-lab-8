b00f52b6b834a482613c42a648b363c838032993
yarynayavor <yourhurt09@gmail.com>
2018-02-09 19:44:21 +0200

    added task1 done

---
+++ homework_07/js/task1.js
@@ -0,0 +1,16 @@
+const maxFloors=20;
+let number=prompt("Enter natural number from 1 to 20:", "5");
+let natural = (!isNaN(number)) && (number>0) && (parseFloat(number)===parseInt(number));
+
+if(natural && number<=maxFloors) {
+	let brick="[~]";
+	let spaces="";
+	for (let i = 1; i <= number; i++) {
+		for(let j=0;j<=(number-i);j++){
+				spaces=" ".repeat(j*3);
+			}
+		console.log(spaces+brick.repeat(i-1)+brick.repeat(i));
+	}
+} else {
+	console.log("Incorrect data. You can enter only natural number from 1 to 20.");
+}
