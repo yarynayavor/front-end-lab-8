e9d041d8fed6324d0528459fc04aee32bae3e8ef
Yaryna Yavor <yourhurt09@gmail.com>
2018-02-24 17:24:13 +0200

    added task2

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -1,68 +1,65 @@
-/**
- * @typedef {Object} CombatHistory
- * @property {wins} number - Number of victories
- * @property {loses} number - Number of defeats
- */
-
-/**
- * @typedef {Object} Stats
- * @property {string} name - The name of fighter
- * @property {number} attack - The amount of attack of fighter
- * @property {number} hp - The total number of health point of fighter
- */
-
-/**
- * @typedef {Object} Fighter
- * @property {function} getName - Return name of the fighter
- * @property {function} fight - Return true if fighter make dmg to enemy, otherwise false
- * @property {function} block - Return true if enemy can block incoming damage, otherwise false (randomly)
- * @property {function} getStats - Retrurn {Stats} of fighter
- * @property {number} getCombatHistory - Return {CombatHistory} of previous fights
- */
-
-/**
- * Pretty print fighter's info
- * @param {Fighter} fighter - The fighter
- * DO NOT MODIFY
- */
-function showResult(fighter) {
-  console.log('Fighter', fighter.getName());
-  console.log('- Combat stats:', fighter.getCombatHistory());
-  console.log('- Properties:', fighter.getStats());
+function fighter(player) {
+    let info = {
+        wins: 0,
+        loses: 0
+    };
+    return {
+        getName: function() {
+            return player.name;
+        },
+        block: function() {
+            return Math.random() >= 0.5;
+        },
+        getStats: function() {
+            return player;
+        },
+        getCombatHistory: function() {
+            return info;
+        },
+        fight: function(enemy) {
+            let blockCheck = enemy.block();
+            let fighterAttack = player.attack;
+            let enemyHp = enemy.getStats().hp;
+            if (!blockCheck) {
+                enemyHp -= fighterAttack;
+                enemy.getStats().hp = enemyHp;
+                if (enemyHp <= 0) {
+                    info.wins++;
+                    enemy.getCombatHistory().loses++;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
 }
 
-/**
- * Your code goes here
- * function foo() { ... }
- */
+function showResult(fighter) {
+    console.log('Fighter', fighter.getName());
+    console.log('- Combat stats:', fighter.getCombatHistory());
+    console.log('- Properties:', fighter.getStats());
+}
 
-/**
- * The following code must be valid after implementation!
- */ 
+let fighter1 = fighter({
+    name: 'John',
+    attack: 100,
+    hp: 100
+});
+let fighter2 = fighter({
+    name: 'Kayn',
+    attack: 50,
+    hp: 300
+});
+let fighter3 = fighter({
+    name: 'Bill',
+    attack: 40,
+    hp: 100
+});
 
-// var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
-// var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
-// var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
+fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
+fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
-// fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
-// fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
-
-// /**
-//  * Fighter John
-//  * - Combat stats: { wins: 1, loses: 0 }
-//  * - Properties: { name: 'John', attack: 100, hp: 100 }
-//  */
-// showResult(fighter1);
-
-// /** Fighter Kayn
-//  * - Combat stats: { wins: 0, loses: 0 }
-//  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
-//  */
-// showResult(fighter2); 
-
-// /**
-//  * Fighter Bill
-//  * - Combat stats: { wins: 0, loses: 1 }
-//  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
-//  */
-// showResult(fighter3);
+showResult(fighter1);
+showResult(fighter2);
+showResult(fighter3);
