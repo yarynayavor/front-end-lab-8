952c5d67771eaf09374afd1c4824234bb37c937d
Yaryna Yavor <yourhurt09@gmail.com>
2018-02-27 23:46:44 +0200

    added simple click

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,26 +1,37 @@
 var rootNode = document.getElementById("root");
 
 function getTreeFromStructure(struct) {
-	let treeList=document.createElement("ul");
-	for (let i = 0; i <struct.length; i++) {
-		let f=document.createElement("li");
-	    if(struct[i].folder) {
-	    	f.innerHTML=`<div class="main"><i class="material-icons folder-close">folder</i><div class="title">${struct[i].title}</div></div>`;
-	    	if(!struct[i].children) {
-	    		f.innerHTML+='<ul class="empty"><li>Folder is empty</li></ul>';
-	    	}
-	    } else {
-	    	f.innerHTML=`<div class="main"><i class="material-icons drive">insert_drive_file</i><div class="title">${struct[i].title}</div></div>`;
-	    }
-	    if(struct[i].children) {
-	    	let child=getTreeFromStructure(struct[i].children);
-	    	f.appendChild(child);
-	    } 
-	    treeList.appendChild(f);
-	}
-	document.body.appendChild(treeList);
-	return treeList;
+    let treeList = document.createElement("ul");
+    treeList.setAttribute("id", "list");
+    for (let i = 0; i < struct.length; i++) {
+        let f = document.createElement("li");
+        if (struct[i].folder) {
+            f.innerHTML = `<div class="main"><i class="material-icons folder-close">folder</i><div class="title">${struct[i].title}</div></div>`;
+            f.className="open";
+            if (!struct[i].children) {
+                f.innerHTML += '<ul class="empty"><li>Folder is empty</li></ul>';
+            }
+        } else {
+        	// if(f.className === "open")
+            f.innerHTML = `<div class="main"><i class="material-icons drive">insert_drive_file</i><div class="title">${struct[i].title}</div></div>`;
+        }
+        if (struct[i].children) {
+            let child = getTreeFromStructure(struct[i].children);
+            // child.className="hide";
+            f.appendChild(child);
+        }
+        treeList.appendChild(f);
+    }
+    document.body.appendChild(treeList);
+    return treeList;
 }
 
-let list=getTreeFromStructure(structure);
+rootNode.addEventListener("click", function(e){
+	e.target.classList.toggle('hide');
+    // if (e.target.classList.contains('close')) {
+
+    // }
+});
+
+let list = getTreeFromStructure(structure);
 rootNode.appendChild(list);
